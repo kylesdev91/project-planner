2e2d201980ad185f135d004b9f8d32f4bf4fd6f8
Kyle Hernandez <kylehernandez@Kyles-MBP.home>
2022-08-08 09:22:44 -0500

    Lecture 58

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="home">
-   Home
+   <div v-if="projects.length"></div>
+   <div v-for="project in projects" :key="project.id">
+    <p>{{ project.title }}</p>
+   </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 
 export default {
   name: 'HomeView',
-  components: {
+  components: {},
+  data() {
+    return {
+      projects: []
+    }
+  },
+   mounted() {
+    fetch('http://localhost:3000/projects')
+    .then(res => res.json())
+    .then(data => this.projects = data)
+    .catch(err => console.log(err.message))
   }
 }
 </script>
